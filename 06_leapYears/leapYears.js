9769f68eabcd2300510abe55e7d3c9b56e8f40ac
Randy <monyerandy15@gmail.com>
2022-06-14 22:40:55 +0100

    First 6 tasks was attempted
    First 6 task has been attempted with a few failed test conditions

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
 
+// #TODO: Test 4 and 6 failed test conditions.
+
+const leapYears = function(year) {
+    if(year % 4 === 0) {
+        return true;
+    } else if(year % 100 === 0 && year % 400 === 0) {
+        return true
+    } else if (year % 100 === 0) {
+        return false;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
